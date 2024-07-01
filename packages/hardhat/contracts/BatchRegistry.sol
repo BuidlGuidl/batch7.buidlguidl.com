ee37aa8d9d0c474266d9653199ec4adff824f78b
Eda Akturk <edakturk96@gmail.com>
2024-07-01 11:07:15 +0300

    Initial commit

---
+++ packages/hardhat/contracts/BatchRegistry.sol
@@ -0,0 +1,71 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BatchRegistry is Ownable {
+
+    uint256 constant CHECK_IN_REWARD = 0.01 ether;
+
+    mapping(address => bool) public allowList;
+    mapping(address => address) public yourContractAddress;
+    bool public isOpen = true;
+    uint256 public checkedInCounter;
+
+    event CheckedIn(bool first, address builder, address checkInContract);
+
+    // Errors
+    error BatchNotOpen();
+    error NotAContract();
+    error NotInAllowList();
+
+
+    modifier batchIsOpen() {
+        if (!isOpen) revert BatchNotOpen();
+        _;
+    }
+
+    modifier senderIsContract() {
+        if (tx.origin == msg.sender) revert NotAContract();
+        _;
+    }
+
+    constructor(address initialOwner) {
+        super.transferOwnership(initialOwner);
+    }
+
+    function updateAllowList(address[] calldata builders, bool[] calldata statuses) public onlyOwner {
+        require(builders.length == statuses.length, "Builders and statuses length mismatch");
+
+        for (uint i = 0; i < builders.length; i++) {
+            allowList[builders[i]] = statuses[i];
+        }
+    }
+
+    function toggleBatchOpenStatus() public onlyOwner {
+        isOpen = !isOpen;
+    }
+
+    function checkIn() public senderIsContract batchIsOpen {
+        if (!allowList[tx.origin]) revert NotInAllowList();
+
+        bool wasFirstTime;
+        if (yourContractAddress[tx.origin] == address(0)) {
+            checkedInCounter++;
+            wasFirstTime = true;
+            (bool success, ) = tx.origin.call{value: CHECK_IN_REWARD}("");
+            require(success, "Failed to send check in reward");
+        }
+
+        yourContractAddress[tx.origin] = msg.sender;
+        emit CheckedIn(wasFirstTime, tx.origin, msg.sender);
+    }
+
+    // Withdraw function for admins in case some builders don't end up checking in
+    function withdraw() public onlyOwner {
+        (bool success, ) = payable(owner()).call{value: address(this).balance}("");
+        require(success, "Failed to withdraw");
+    }
+
+    receive() external payable {}
+}
---
+++ packages/nextjs/app/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Link from "next/link";
+import type { NextPage } from "next";
+import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div className="px-5">
+          <h1 className="text-center">
+            <span className="block text-2xl mb-2">Welcome to</span>
+            <span className="block text-4xl font-bold">Batch X</span>
+          </h1>
+          <p className="text-center text-lg">Get started by taking a look at your batch GitHub repository.</p>
+          <p className="text-lg flex gap-2 justify-center">
+            <span className="font-bold">Checked in builders count:</span>
+            <span>To Be Implemented</span>
+          </p>
+        </div>
+
+        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <BugAntIcon className="h-8 w-8 fill-secondary" />
+              <p>
+                Tinker with your smart contract using the{" "}
+                <Link href="/debug" passHref className="link">
+                  Debug Contracts
+                </Link>{" "}
+                tab.
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+              <p>
+                Explore your local transactions with the{" "}
+                <Link href="/blockexplorer" passHref className="link">
+                  Block Explorer
+                </Link>{" "}
+                tab.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
