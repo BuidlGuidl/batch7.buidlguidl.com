f25a8c89654ccff8200adc06b2d12464d7250481
Derrek <80121818+derrekcoleman@users.noreply.github.com>
2024-07-04 11:35:57 -0600

    Add contract address and update batch number (#10)

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Batch X</span>
+            <span className="block text-4xl font-bold">Batch 7</span>
           </h1>
           <p className="text-center text-lg">Get started by taking a look at your batch GitHub repository.</p>
           <p className="text-lg flex gap-2 justify-center">
